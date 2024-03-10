74a13b6dbcc8793a14a3b023e907142066165ea3
aisanRodel <gogogo990099@gmail.com>
2024-03-10 13:39:03 +0900

    Including Module Path
    모듈 path 설정

--- Source/History/History.Build.cs
+++ Source/History/History.Build.cs
@@ -7,6 +7,8 @@ public class History : ModuleRules
 	public History(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicIncludePaths.AddRange(new string[] { "History" });
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
