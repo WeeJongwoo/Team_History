28fb1c32abac194a3688647e0127216e39fd6d54
asianRodel <gogogo990099@gmail.com>
2024-03-15 02:23:27 +0900

    Create Heritage Actor Base Class
    [진행사항]
     - 문화재 액터용 컴포넌트 클래스 삭제
     - 문화재 액터 베이스 클래스 생성
     - 문화재 범위에 따라 UI 컨트롤
    
    [추가작업사항]
     - UI에 표시되는 정보의 로드(데이터 테이블 이용)
     - UI 표시 위치 조정
    
    [논의사항]
     - UI 디자인과 위치
    
    문화재에 필요한 요소들을 컴포넌트로 분리하는 것이 크게 의미가 없단 걸 알게 되어 하나의 베이스 액터 클래스에 포함시켜 만들었다.
    
    현재 범위에 진입 시 UI가 표시되고 범위를 벗어나면 UI가 사라진다.
    다만 UI에 표시되는 정보는 예시로서 데이터 테이블을 만들어 그곳에서 데이터를 받아오게 하는 작업이 추가로 필요하다. 또한 UI의 디자인과 표시 방식에 대해서는 추가 논의가 필요하다.

---
+++ Source/History/Heritage/CHeritageActorBase.cpp
@@ -0,0 +1,78 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Heritage/CHeritageActorBase.h"
+#include "UI/CHeritageWidgetComponent.h"
+#include "UI/CHeritageInfoWidget.h"
+#include "Components/SphereComponent.h"
+
+// Sets default values
+ACHeritageActorBase::ACHeritageActorBase()
+{
+	InteractiveAreaRadius = 100.0f;
+	bIsFirstInteraction = true;
+
+	HeritageMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("HeritageMash"));
+	RootComponent = HeritageMeshComponent;
+
+	InteractiveArea = CreateDefaultSubobject<USphereComponent>(TEXT("HeritageArea"));
+	InteractiveArea->SetupAttachment(RootComponent);
+	InteractiveArea->SetSphereRadius(InteractiveAreaRadius);
+	InteractiveArea->OnComponentBeginOverlap.AddDynamic(this, &ACHeritageActorBase::OnInteractionTriggerBeginOverlap);
+	InteractiveArea->OnComponentEndOverlap.AddDynamic(this, &ACHeritageActorBase::OnInteractionTriggerEndOverlap);
+
+	HeritageInfo = CreateDefaultSubobject<UCHeritageWidgetComponent>(TEXT("HeritageInfo"));
+	HeritageInfo->SetupAttachment(RootComponent);
+
+	static ConstructorHelpers::FClassFinder<UCHeritageInfoWidget> HeritageInfoWidgetRef(TEXT("/Game/Project_History/UI/WB_HeritageInfo.WB_HeritageInfo_C"));
+	if (HeritageInfoWidgetRef.Class)
+	{
+		HeritageInfo->SetWidgetClass(HeritageInfoWidgetRef.Class);
+		HeritageInfo->SetWidgetSpace(EWidgetSpace::Screen);
+		HeritageInfo->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	}
+}
+
+// Called when the game starts or when spawned
+void ACHeritageActorBase::BeginPlay()
+{
+	Super::BeginPlay();
+
+	HeritageInfo->SetVisibility(false);
+}
+
+void ACHeritageActorBase::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+
+	InteractiveArea->SetSphereRadius(InteractiveAreaRadius);
+}
+
+void ACHeritageActorBase::OnInteractionTriggerBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	InteractHeritage();
+}
+
+void ACHeritageActorBase::OnInteractionTriggerEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	UE_LOG(LogTemp, Log, TEXT("End Overlap"));
+
+	HeritageInfo->SetVisibility(false);
+}
+
+void ACHeritageActorBase::InteractHeritage()
+{
+	if (bIsFirstInteraction == true)
+	{
+		UE_LOG(LogTemp, Log, TEXT("First OnInteractionTriggerBeginOverlap"));
+		bIsFirstInteraction = false;
+
+
+	}
+	else
+	{
+		UE_LOG(LogTemp, Log, TEXT("OnInteractionTriggerBeginOverlap"));
+	}
+	HeritageInfo->SetVisibility(true);
+}
+
